dc2536cfb1f3ef12092c2e2a28a455e56ed6e7df
RimshaIshtiaq98 <36766647+RimshaIshtiaq98@users.noreply.github.com>
2018-05-11 21:32:39 +0500

    Add files via upload

---
+++ Asignment.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asignment;
+
+public class Asignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HMM hmm = new HMM();
+      
+        String add;
+        add ="E:\\Zip file\\asignment (1)~\\asignment\\src\\asignment\\HotCold.txt";  // change path for another file
+        hmm.readFile(add,2,9); // address,no of states,length of obsevation
+        hmm.forward_algo(2, 9); // in this and below function 1st parameter is no of states and 2nd is length of observation 
+       hmm.viterbi_Algo(2, 9);
+        
+       
+    }
+    
+}
